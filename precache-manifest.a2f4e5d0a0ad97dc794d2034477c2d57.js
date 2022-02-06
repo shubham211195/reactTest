c0fd54068915ceb2c61f016b9c1a4604fd98deb7
shubham211195 <Shubaryan211195@gmail.com>
2022-02-06 20:25:13 +0530

    Updates

---
+++ precache-manifest.a2f4e5d0a0ad97dc794d2034477c2d57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37c719f84086423cf5aa",
+    "url": "/reactTest/static/css/main.8e478bcb.chunk.css"
+  },
+  {
+    "revision": "37c719f84086423cf5aa",
+    "url": "/reactTest/static/js/main.8dfa3e74.chunk.js"
+  },
+  {
+    "revision": "2ce7ab29404c74777852",
+    "url": "/reactTest/static/js/runtime~main.3be99dba.js"
+  },
+  {
+    "revision": "71d7194958f9367e0e6e",
+    "url": "/reactTest/static/js/2.de6ff777.chunk.js"
+  },
+  {
+    "revision": "24fa9ff8b7bf3783a38e3616b6bb0a7c",
+    "url": "/reactTest/index.html"
+  }
+];
